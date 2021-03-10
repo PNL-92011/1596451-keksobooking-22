baa83575469eb7149632d6ecb3dfd5727dda9d3d
Mary <maria79264163641@gmail.com>
2021-03-10 15:08:51 +0300

    Card.js: add checking of value and removing field if no data.

--- js/card.js
+++ js/card.js
@@ -1,25 +1,29 @@
+const photoSize = {
+  WIDTH: 45,
+  HEIGHT: 40,
+};
+
 const similarListElement = document.querySelector('.map__canvas');
 const similarAdTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
 
-const photoSize = {
-  WIDTH: 45,
-  HEIGHT: 40,
-};
+/**
+ * Функция отображения карточки объявления
+ * @return {object} карточка объявления по шаблону
+ */
 
-// Функция отображения карточки объявления
 const renderCard = (elem) => {
 
   const adElement = similarAdTemplate.cloneNode(true);
 
   // функция вывода иконок преимуществ в карточку объявления
-  const getFeaturesNumber = () => {
+  const getFeaturesNumber = (FEATURES) => { // используем параметр в функции, чтобы она была объявлена не глобально, а локально!
     if (elem.offer.features) {
-      elem.offer.features.forEach((item, FEATURES) => {
+      elem.offer.features.forEach((feature) => { // объявляем параметр (feature) и используем его в цикле
         const featureItem = document.createElement('li');
-        featureItem.classList.add('popup__feature', `popup__feature--${elem.offer.features[FEATURES]}`);
+        featureItem.classList.add('popup__feature', `popup__feature--${[feature]}`);
         adElement.querySelector('.popup__features').append(featureItem);
       });
     } else { // Если данных для заполнения не хватает, соответствующий блок в карточке скрывается
@@ -28,15 +32,14 @@ const renderCard = (elem) => {
   };
 
   // функция вывода фото в карточку объявления
-  const getPhotosNumber = () => {
+  const getPhotosNumber = (PHOTOS) => {
     if (elem.offer.photos) {
       elem.offer.photos.forEach((photo) => {
         const photoItem = document.createElement('img');
         photoItem.src = photo;
-        // photoItem.src = elem.offer.photos[PHOTOS];
         photoItem.alt = 'Фотография жилья';
         photoItem.style.width = `${photoSize.WIDTH}px`;
-        photoItem.style.width = `${photoSize.HEIGHT}px`;
+        photoItem.style.height = `${photoSize.HEIGHT}px`;
         photoItem.classList.add('popup__photo');
         adElement.querySelector('.popup__photos').append(photoItem);
       });
@@ -59,8 +62,34 @@ const renderCard = (elem) => {
   adElement.querySelector('.popup__photos').textContent = '';
   getPhotosNumber();
 
+
+  /**
+  * Проверка наличия данных и удаление поля в случае отсутствия данных
+  * @param {object} данные для поля
+  * @param {object} селектор
+  * @return {*} либо поле с данными, либо поля нет
+  */
+  const insertValue = (value, selector) => {
+    if (value) {
+      adElement.querySelector(selector).textContent = value;
+    } else {
+      adElement.querySelector(selector).remove();
+    }
+  }
+
+  insertValue(elem.offer.title, '.popup__title');
+  insertValue(elem.offer.address, '.popup__text--address');
+  insertValue(`${elem.offer.price} ₽/ночь`, '.popup__text--price');
+  insertValue(elem.offer.type, '.popup__type');
+  insertValue(`${elem.offer.rooms} комнат(ы) для ${elem.offer.guests} гостей`, '.popup__text--capacity');
+  insertValue(`Заезд после ${elem.offer.checkin}, выезд до ${elem.offer.checkout}`, '.popup__text--time');
+  insertValue(elem.offer.description, '.popup__description');
+  insertValue(elem.author.avatar, '.popup__avatar');
+
+
   return adElement;
 };
+
 
 export {
   similarListElement,
